bc3275a6587fdfd85507cfe767286fff2ad856e4
Sukanta Das <arnobh84@gmail.com>
2023-01-31 20:02:31 +0600

    from setup completed

--- src/Authentication/SignUp.jsx
+++ src/Authentication/SignUp.jsx
@@ -1,15 +1,30 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import uploadImage from "../helper/uploadImage";
 import brandLogo from "../images/c-logo.png";
 
 const SignUp = () => {
   const [selectedImage, setSelectedImage] = useState();
+
+  const handleSignUp = async (e) => {
+    e.preventDefault();
+    const name = e.target.name.value;
+    const email = e.target.email.value;
+    const password = e.target.password.value;
+    const profilePicture = e.target.profilePicture.files[0];
+    const formData = new FormData();
+    formData.append("image", profilePicture);
+    // const imageUri = await uploadImage(formData);
+
+    console.log(name, email, password, profilePicture);
+  };
 
   const imageChange = (e) => {
     if (e.target.files && e.target.files.length > 0) {
       setSelectedImage(e.target.files[0]);
     }
   };
+
   return (
     <div>
       <div class="max-w-screen-xl m-0 sm:m-10 bg-white shadow sm:rounded-lg flex justify-center flex-1">
@@ -33,46 +48,55 @@ const SignUp = () => {
           <div class="mt-5 flex flex-col items-center">
             <div class="w-full flex-1 mt-3">
               <div class="mx-auto max-w-xs">
-                <input
-                  class="w-full px-8 py-4 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white"
-                  type="text"
-                  placeholder="Name"
-                />
-                <input
-                  class="w-full px-8 py-4 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white mt-5"
-                  type="email"
-                  placeholder="Email"
-                />
-                <input
-                  class="w-full px-8 py-4 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white mt-5"
-                  type="password"
-                  placeholder="Password"
-                />
-                <div className="  mt-5">
-                  <label class="block">
-                    <span class="sr-only">Choose File</span>
-                    <input
-                      onChange={imageChange}
-                      type="file"
-                      class="block w-full text-sm text-gray-500 file:mr-4 file:py-2 file:px-4 file:rounded-full file:border-0 file:text-sm file:font-semibold file:bg-blue-50 file:text-blue-700 hover:file:bg-blue-100 "
-                    />
-                  </label>
-                </div>
-                <button class="mt-5 tracking-wide font-semibold bg-indigo-500 text-gray-100 w-full py-4 rounded-lg hover:bg-indigo-700 transition-all duration-300 ease-in-out flex items-center justify-center focus:shadow-outline focus:outline-none">
-                  <svg
-                    class="w-6 h-6 -ml-2"
-                    fill="none"
-                    stroke="currentColor"
-                    stroke-width="2"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
+                <form onSubmit={handleSignUp}>
+                  <input
+                    class="w-full px-8 py-4 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white"
+                    type="text"
+                    placeholder="Name"
+                    name="name"
+                  />
+                  <input
+                    class="w-full px-8 py-4 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white mt-5"
+                    type="email"
+                    placeholder="Email"
+                    name="email"
+                  />
+                  <input
+                    class="w-full px-8 py-4 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white mt-5"
+                    type="password"
+                    placeholder="Password"
+                    name="password"
+                  />
+                  <div className="  mt-5">
+                    <label class="block">
+                      <span class="sr-only">Choose File</span>
+                      <input
+                        onChange={imageChange}
+                        type="file"
+                        name="profilePicture"
+                        class="block w-full text-sm text-gray-500 file:mr-4 file:py-2 file:px-4 file:rounded-full file:border-0 file:text-sm file:font-semibold file:bg-blue-50 file:text-blue-700 hover:file:bg-blue-100 "
+                      />
+                    </label>
+                  </div>
+                  <button
+                    class="mt-5 tracking-wide font-semibold bg-indigo-500 text-gray-100 w-full py-4 rounded-lg hover:bg-indigo-700 transition-all duration-300 ease-in-out flex items-center justify-center focus:shadow-outline focus:outline-none"
+                    type="submit"
                   >
-                    <path d="M16 21v-2a4 4 0 00-4-4H5a4 4 0 00-4 4v2" />
-                    <circle cx="8.5" cy="7" r="4" />
-                    <path d="M20 8v6M23 11h-6" />
-                  </svg>
-                  <span class="ml-3">Sign Up</span>
-                </button>
+                    <svg
+                      class="w-6 h-6 -ml-2"
+                      fill="none"
+                      stroke="currentColor"
+                      stroke-width="2"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                    >
+                      <path d="M16 21v-2a4 4 0 00-4-4H5a4 4 0 00-4 4v2" />
+                      <circle cx="8.5" cy="7" r="4" />
+                      <path d="M20 8v6M23 11h-6" />
+                    </svg>
+                    <span class="ml-3">Sign Up</span>
+                  </button>
+                </form>
                 <p className="mt-3">
                   All ready user{" "}
                   <Link to="/signIn">
